5a1ea254399d21fe7172d39fe9124ee73bdb06a6
Amir <amirrllotfi@gmail.com>
2025-03-05 12:13:19 +0100

    create Auth usecase and implementation

---
+++ lib/auth/domain/usecase/auth_usecase.dart
@@ -0,0 +1,21 @@
+import 'package:auth_using_firbase_riverpod/auth/data/model/user_model.dart';
+import 'package:auth_using_firbase_riverpod/auth/domain/repository/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthUsecase {
+  final AuthRepository _authRepository;
+
+  AuthUsecase(this._authRepository);
+
+  Future<User?> signInWithEmailAndPassword(UserModel user) async {
+    return await _authRepository.signInWithEmailAndPassword(user);
+  }
+
+  Future<User?> signUpWithEmailAndPassword(UserModel user) async {
+    return await _authRepository.signUpWithEmailAndPassword(user);
+  }
+
+  Future<void> logout() async {
+    return await _authRepository.signOut();
+  }
+}
